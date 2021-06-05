aaa667622535378fb0c698f731bcbd03f52b6fce
Sophie Krag <sophie.9@live.nl>
2021-06-05 15:19:59 +0200

    Initialised routes and views folders

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
+const logger = require("morgan");
 
 require("./config/db.config");
 
 const app = express();
+
+app.use(logger("dev"));
+
+// eslint-disable-next-line no-undef
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+//Routes
+app.use("/", require("./routes/index"));
 
 /* eslint-disable no-undef */
 app.listen(
