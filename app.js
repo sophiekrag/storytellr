16ab6d0e6f5721dbfc94da82f126810d414f16fd
Sophie Krag <sophie.9@live.nl>
2021-06-05 14:42:12 +0200

    Set up connection to database

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 
+require("./config/db.config");
+
 const app = express();
 
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 app.listen(
   process.env.PORT,
   console.log(`Server running on ${process.env.PORT}`)
 );
+/* eslint-enable no-undef */
