c31d60e44f3011178e242ca51c3c2a5ab930ff4d
Sophie Krag <sophie.9@live.nl>
2021-06-07 09:29:42 +0200

    Initialized signup model, route, view

--- app.js
+++ app.js
@@ -4,18 +4,31 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 
+//setup database
 require("./config/db.config");
+
+//Routes
+const indexRouter = require("./routes/index.routes");
+const authRouter = require("./routes/auth.routes");
 
 const app = express();
 
 app.use(logger("dev"));
 
+//express view engine setup
 // eslint-disable-next-line no-undef
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-//Routes
-app.use("/", require("./routes/index"));
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes middleware
+app.use("/", indexRouter);
+app.use("/", authRouter);
 
 /* eslint-disable no-undef */
 app.listen(
