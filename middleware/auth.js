29e2b76d7e927e533249b967de812cce5aba746f
Sophie Krag <sophie.9@live.nl>
2021-06-14 10:59:43 +0200

    Add and noAuth view

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ function checkAuth(req, res, next) {
   if (req.session.currentUser) {
     next();
   } else {
-    res.send("You are not authorized to view this page");
+    res.render("../views/noAuth.hbs");
   }
 }
 
