751faa992cbff430e84597b7313bb1c26629e49a
Sophie Krag <sophie.9@live.nl>
2021-06-17 20:28:06 +0200

    Changed the index page, someone who is logged in sees something different

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res) => res.render("index"));
+router.get("/", (req, res) => {
+  res.render("index", {
+    userInSession: req.session.currentUser,
+  });
+});
 
 module.exports = router;
